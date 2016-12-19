fe5bb14a3fea1a1d90822a46e4fc44528ba421e6
moiseencoo <95varya@mail.ru>
2016-12-19 19:21:17 +0500

    spec.html complete

--- js/main.js
+++ js/main.js
@@ -124,3 +124,31 @@ $(function () {
 		, navigationButtonsVisible: true
 	});
 });
+
+// Switch Tabs
+$('ul.ul-spec').each(function(){
+          var $active, $content, $links = $(this).find('a');
+
+          $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+          $active.addClass('active');
+
+          $content = $($active[0].hash);
+
+          // Hide the remaining content
+          $links.not($active).each(function () {
+            $(this.hash).hide();
+          });
+
+          $(this).on('click', 'a', function(e){
+            $active.removeClass('active');
+            $content.hide();
+
+            $active = $(this);
+            $content = $(this.hash);
+
+            $active.addClass('active');
+            $content.show();
+
+            e.preventDefault();
+          });
+        });
